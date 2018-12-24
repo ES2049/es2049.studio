c1710b8deb24979b569a7db4169f19965c5ca85b
沐飏 <gezhen.gz@alibaba-inc.com>
2018-12-24 18:38:23 +0800

    larger font

--- work-show/emotion/precache-manifest.fd36837dc778299231ad1e2f0dfa847a.js
+++ work-show/emotion/precache-manifest.fd36837dc778299231ad1e2f0dfa847a.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/work-show/emotion/static/js/runtime~main.eadafe30.js"
   },
   {
-    "revision": "35a2fd0ac6dbdc124a6d",
-    "url": "/work-show/emotion/static/js/main.35a2fd0a.chunk.js"
+    "revision": "2b13e3d9e57981f650ba",
+    "url": "/work-show/emotion/static/js/main.2b13e3d9.chunk.js"
   },
   {
     "revision": "7356278b44a7f897a283",
     "url": "/work-show/emotion/static/js/1.7356278b.chunk.js"
   },
   {
-    "revision": "35a2fd0ac6dbdc124a6d",
+    "revision": "2b13e3d9e57981f650ba",
     "url": "/work-show/emotion/static/css/main.65d3bbd9.chunk.css"
   },
   {
-    "revision": "07897889214a83dd1baa7dd70f097798",
+    "revision": "1211f3abe7b22664b7a36319b111be99",
     "url": "/work-show/emotion/index.html"
   }
 ];
